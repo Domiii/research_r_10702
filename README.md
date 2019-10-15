a8b79dd7754a2c2e18a1783e1bb51a60a20667f3
Domiii <dominik.seifert@gmail.com>
2019-10-15 18:40:45 +0800

    - setup venv

---
+++ README.md
@@ -0,0 +1,6 @@
+## Steps taken
+
+1. Build a basic Google Web App, using clang (in `gs/` folder)
+   * Can list specific files on website
+1. Decided to use python + Google Drive API instead
+   1. Setup python venv https://realpython.com/python-virtual-environments-a-primer/
--- py/main.py
+++ py/main.py
@@ -1,30 +1,21 @@
 from __future__ import print_function
-import pickle
-import os.path
-from googleapiclient.discovery import build
-from google_auth_oauthlib.flow import InstalledAppFlow
-from google.auth.transport.requests import Request
 
 import os
 from os import path
+
 from gutil import getFilesInFolder, exportFileCsv
-from drive import getOrCreateService
+# from drive import getOrCreateService
 
 csvFolder = f'{path.dirname(__file__)}/_files'
 
 # TODO: setup virtualenv - https://towardsdatascience.com/all-you-need-to-know-about-python-virtual-environments-9b4aae690f97
 
-def main():
-  service = getOrCreateService()
-
-  # Query files from drive
-  # https://drive.google.com/drive/u/0/folders/16SQCIy97DRDsRAhpKBilbMh8hlazQAhZ
-  driveFolderId = '16SQCIy97DRDsRAhpKBilbMh8hlazQAhZ'
-  files = getFilesInFolder(driveFolderId, mimeType = 'application/vnd.google-apps.spreadsheet')
-
+def exportFiles(files):
   # create folder, if it does not exist yet
-  os.mkdir(csvFolder)
+  if not path.exists(csvFolder):
+    os.mkdir(csvFolder)
 
+  # export files
   if not files:
     print('No files found.')
   else:
@@ -32,6 +23,17 @@ def main():
     for f in files:
       print(u'{0} ({1})'.format(f['name'], f['id']))
       exportFileCsv(csvFolder, f)
+
+def main():
+  # service = getOrCreateService()
+
+  # Query files from drive
+  # https://drive.google.com/drive/u/0/folders/16SQCIy97DRDsRAhpKBilbMh8hlazQAhZ
+  driveFolderId = '16SQCIy97DRDsRAhpKBilbMh8hlazQAhZ'
+  files = getFilesInFolder(driveFolderId, mimeType='application/vnd.google-apps.spreadsheet')
+
+  # export files
+  exportFiles(files)
 
 
 if __name__ == '__main__':
